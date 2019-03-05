605b763e31ea1a5062a9503ee7670f3cdbfac475
Rafael Augusto Grube <rafaelgrube@gmail.com>
2019-03-05 01:08:44 -0300

    Redux for the places App

--- store/reducers/places.js
+++ store/reducers/places.js
@@ -12,6 +12,37 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_PLACE:
+      return {
+        ...state,
+        places: state.places.concat({
+          key: Math.random(),
+          name: action.placeName,
+          image: {
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuWwWO6vMH1V2HdIO8J1dMjI10qHIRHlsbrZIw6vf3967t5Wowug',
+          },
+        }),
+      };
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter((place) => {
+          return place.key !== state.selectedPlace.key;
+        }),
+        selectedPlace: null,
+      };
+    case SELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: state.places.find((place) => {
+          return place.key === action.placeKey;
+        }),
+      };
+    case DESELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: null,
+      };
     default:
       return state;
   }
